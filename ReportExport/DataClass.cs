d07b4844ae0bf7daf75c3d00219198e981281fd2
vupt9 <vupt9@DESKTOP-BVKLJM6>
2018-04-16 17:01:47 +0700

    Initital

---
+++ ReportExport/DataClass.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportExport 
+{
+    class Data
+    {
+        public String createDate { get; set; }
+        public String dealerCode { get; set; }
+        public String repairNo { get; set; }
+        public String typeCD { get; set; }
+        public String wrNo { get; set; }
+        public String frameNo { get; set; }
+        public String eNo { get; set; }
+        public String plateNo { get; set; }
+        public int km { get; set; }
+        public String dop { get; set; }
+        public String cusName { get; set; }
+        public String cusAddress { get; set; }
+        public String phone { get; set; }
+        public String pCode { get; set; }
+        public String pName { get; set; }
+        public String dCode { get; set; }
+        public String dName { get; set; }
+        public String empReceive { get; set; }
+        public String empRepair { get; set; }
+        public String fuel { get; set; }
+        public String printed { get; set; }
+        public String dateIn { get; set; }
+        public String dateOut { get; set; }
+        public String modelName { get; set; }
+        public String cusRequest { get; set; }
+        public String insResult { get; set; }
+        public String beforeRepair { get; set; }
+        public List<Item> items { get; set; }
+        public class Item
+        {
+            public int discountP { get; set; }
+            public int discountW { get; set; }
+            public String repairDesc { get; set; }
+            public int rate0 { get; set; }
+            public String partNo { get; set; }
+            public String partName { get; set; }
+            public int rate1 { get; set; }
+            public int qty { get; set; }
+            public String asNo { get; set; }
+            public String checkedStatus { get; set; }
+            public String typeNo { get; set; }
+        }
+
+    }
+}
---
+++ ReportExport/MainForm.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace ReportExport
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblURL = new System.Windows.Forms.Label();
+            this.lblUser = new System.Windows.Forms.Label();
+            this.lblPwd = new System.Windows.Forms.Label();
+            this.lblRepairNo = new System.Windows.Forms.Label();
+            this.lblCreateFrom = new System.Windows.Forms.Label();
+            this.dtpFrom = new System.Windows.Forms.DateTimePicker();
+            this.dtpTo = new System.Windows.Forms.DateTimePicker();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnView = new System.Windows.Forms.Button();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.lblDBName = new System.Windows.Forms.Label();
+            this.txtDBName = new System.Windows.Forms.TextBox();
+            this.txtRepairNo = new System.Windows.Forms.TextBox();
+            this.dgvData = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(369, 128);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Export";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnExportClick);
+            // 
+            // lblURL
+            // 
+            this.lblURL.AutoSize = true;
+            this.lblURL.Location = new System.Drawing.Point(12, 18);
+            this.lblURL.Name = "lblURL";
+            this.lblURL.Size = new System.Drawing.Size(47, 13);
+            this.lblURL.TabIndex = 1;
+            this.lblURL.Text = "DB URL";
+            // 
+            // lblUser
+            // 
+            this.lblUser.AutoSize = true;
+            this.lblUser.Location = new System.Drawing.Point(30, 52);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(29, 13);
+            this.lblUser.TabIndex = 2;
+            this.lblUser.Text = "User";
+            // 
+            // lblPwd
+            // 
+            this.lblPwd.AutoSize = true;
+            this.lblPwd.Location = new System.Drawing.Point(472, 52);
+            this.lblPwd.Name = "lblPwd";
+            this.lblPwd.Size = new System.Drawing.Size(53, 13);
+            this.lblPwd.TabIndex = 3;
+            this.lblPwd.Text = "Password";
+            // 
+            // lblRepairNo
+            // 
+            this.lblRepairNo.AutoSize = true;
+            this.lblRepairNo.Location = new System.Drawing.Point(1, 87);
+            this.lblRepairNo.Name = "lblRepairNo";
+            this.lblRepairNo.Size = new System.Drawing.Size(58, 13);
+            this.lblRepairNo.TabIndex = 4;
+            this.lblRepairNo.Text = "Repair No.";
+            // 
+            // lblCreateFrom
+            // 
+            this.lblCreateFrom.AutoSize = true;
+            this.lblCreateFrom.Location = new System.Drawing.Point(461, 87);
+            this.lblCreateFrom.Name = "lblCreateFrom";
+            this.lblCreateFrom.Size = new System.Drawing.Size(64, 13);
+            this.lblCreateFrom.TabIndex = 5;
+            this.lblCreateFrom.Text = "Create Date";
+            // 
+            // dtpFrom
+            // 
+            this.dtpFrom.CustomFormat = "dd/MM/yyyy HH:mm:ss";
+            this.dtpFrom.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpFrom.Location = new System.Drawing.Point(545, 87);
+            this.dtpFrom.Name = "dtpFrom";
+            this.dtpFrom.Size = new System.Drawing.Size(155, 20);
+            this.dtpFrom.TabIndex = 7;
+            // 
+            // dtpTo
+            // 
+            this.dtpTo.CustomFormat = "dd/MM/yyyy HH:mm:ss";
+            this.dtpTo.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpTo.Location = new System.Drawing.Point(706, 87);
+            this.dtpTo.Name = "dtpTo";
+            this.dtpTo.Size = new System.Drawing.Size(148, 20);
+            this.dtpTo.TabIndex = 8;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(450, 128);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 9;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnView
+            // 
+            this.btnView.Location = new System.Drawing.Point(288, 128);
+            this.btnView.Name = "btnView";
+            this.btnView.Size = new System.Drawing.Size(75, 23);
+            this.btnView.TabIndex = 10;
+            this.btnView.Text = "View";
+            this.btnView.UseVisualStyleBackColor = true;
+            this.btnView.Click += new System.EventHandler(this.btnView_Click);
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(69, 15);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(293, 20);
+            this.txtURL.TabIndex = 11;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(545, 49);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(309, 20);
+            this.txtPassword.TabIndex = 12;
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(69, 49);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(293, 20);
+            this.txtUser.TabIndex = 13;
+            // 
+            // lblDBName
+            // 
+            this.lblDBName.AutoSize = true;
+            this.lblDBName.Location = new System.Drawing.Point(472, 19);
+            this.lblDBName.Name = "lblDBName";
+            this.lblDBName.Size = new System.Drawing.Size(53, 13);
+            this.lblDBName.TabIndex = 14;
+            this.lblDBName.Text = "DB Name";
+            // 
+            // txtDBName
+            // 
+            this.txtDBName.Location = new System.Drawing.Point(545, 15);
+            this.txtDBName.Name = "txtDBName";
+            this.txtDBName.Size = new System.Drawing.Size(309, 20);
+            this.txtDBName.TabIndex = 15;
+            // 
+            // txtRepairNo
+            // 
+            this.txtRepairNo.Location = new System.Drawing.Point(69, 84);
+            this.txtRepairNo.Name = "txtRepairNo";
+            this.txtRepairNo.Size = new System.Drawing.Size(293, 20);
+            this.txtRepairNo.TabIndex = 16;
+            // 
+            // dgvData
+            // 
+            this.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvData.Location = new System.Drawing.Point(15, 157);
+            this.dgvData.Name = "dgvData";
+            this.dgvData.Size = new System.Drawing.Size(839, 281);
+            this.dgvData.TabIndex = 17;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(874, 448);
+            this.Controls.Add(this.dgvData);
+            this.Controls.Add(this.txtRepairNo);
+            this.Controls.Add(this.txtDBName);
+            this.Controls.Add(this.lblDBName);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtURL);
+            this.Controls.Add(this.btnView);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.dtpTo);
+            this.Controls.Add(this.dtpFrom);
+            this.Controls.Add(this.lblCreateFrom);
+            this.Controls.Add(this.lblRepairNo);
+            this.Controls.Add(this.lblPwd);
+            this.Controls.Add(this.lblUser);
+            this.Controls.Add(this.lblURL);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblURL;
+        private System.Windows.Forms.Label lblUser;
+        private System.Windows.Forms.Label lblPwd;
+        private System.Windows.Forms.Label lblRepairNo;
+        private System.Windows.Forms.Label lblCreateFrom;
+        private System.Windows.Forms.DateTimePicker dtpFrom;
+        private System.Windows.Forms.DateTimePicker dtpTo;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnView;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.Label lblDBName;
+        private System.Windows.Forms.TextBox txtDBName;
+        private System.Windows.Forms.TextBox txtRepairNo;
+        private System.Windows.Forms.DataGridView dgvData;
+    }
+}
