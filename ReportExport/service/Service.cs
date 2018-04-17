ffaa385f5443a820d87f38b22b795c620c3ba436
Thành Phạm <longht3005@gmail.com>
2018-04-18 02:43:40 +0700

    fix review

--- ReportExport/service/Service.cs
+++ ReportExport/service/Service.cs
@@ -130,7 +130,7 @@ namespace ReportExport
             }
 
             String pathExport = "D:\\";
-            using (var file = new FileStream(pathExport + "Workbook" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".xlsx", FileMode.CreateNew, FileAccess.ReadWrite))
+            using (var file = new FileStream(pathExport + "ExportSCTT_" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".xlsx", FileMode.CreateNew, FileAccess.ReadWrite))
             {
                 workbook.Write(file);
             }
